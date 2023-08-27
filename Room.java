1e084ad371bd94c547cafb76fbc6fd24d1071ba2
Isaque Do Nascimento <isaquemask@gmail.com>
2023-08-27 12:35:03 -0300

    Questão 8.8 alterar o metodo setExit e corrigir o getExit tbm, feito 80% da questão só precisa corrigir o hashMap para que ele de a resposta correta

--- Room.java
+++ Room.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Class Room - a room in an adventure game.
  *
@@ -15,10 +17,7 @@
 public class  Room
 {
     private String description;
-    private Room northExit;
-    private Room southExit;
-    private Room eastExit;
-    private Room westExit;
+    private HashMap<String, Room>exits;
 
     /**
      * Create a room described "description". Initially, it has
@@ -29,6 +28,7 @@ public class  Room
     public Room(String description)
     {
         this.description = description;
+        exits = new HashMap<>();
     }
 
     /**
@@ -39,21 +39,19 @@ public class  Room
      * @param south The south exit.
      * @param west The west exit.
      */
-    public void setExits(Room north, Room east, Room south, Room west) 
+
+    /**
+     * Define an exit from this room.
+     * @param direction The direction of the exit.
+     * @param neighbor The room in the given direction.
+     */
+
+
+    public void setExit(String direction, Room neighbor)
     {
-        if(north != null) {
-            northExit = north;
-        }
-        if(east != null) {
-            eastExit = east;
-        }
-        if(south != null) {
-            southExit = south;
-        }
-        if(west != null) {
-            westExit = west;
-        }
+        exits.put(direction, neighbor);
     }
+
 
 
     /**
@@ -63,27 +61,14 @@ public class  Room
     {
         return description;
     }
-    public Room getNorthExit() {return northExit; }
-    public Room getSouthExit() {return southExit; }
-    public Room getEastExit() {return eastExit; }
-    public Room getWestExit() {return westExit; }
 
-     public void getExitString() {
-         System.out.println(description);
-         System.out.println("Voce está " + description);
-         System.out.print("Exits: ");
-         if(northExit != null) {
-             System.out.print("north ");
+
+     public void getExit() {
+         if (exits != null) {
+             System.out.println(exits);
          }
-         if(eastExit != null) {
-             System.out.print("east ");
+        else{
+             System.out.println("Não há saida");
          }
-         if(southExit!= null) {
-             System.out.print("south ");
-         }
-         if(westExit != null) {
-             System.out.print("west ");
-         }
-         System.out.println();
      }
 }
